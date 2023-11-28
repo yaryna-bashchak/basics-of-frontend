61999b723e2750bf4fa6256b3651d9c00c20bb90
yaryna-bashchak <yaryna.bashchak@gmail.com>
2023-11-28 15:24:12 +0200

    initialize react project for task 1 of lab 7

---
+++ lab7/task1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Content from './components/Content';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
